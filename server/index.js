25f0fb9b01e1a192d61811c9adfc4308dd8be9fa
dani.edo <dani.nurafandi@gomodo.tech>
2020-03-06 15:03:09 +0700

    builded

--- server/index.js
+++ server/index.js
@@ -11,6 +11,15 @@ app.use(cors());
 const post = require("./routes/api/posts");
 app.use("/api/posts", post);
 
+// Handle production
+if (process.env.NODE_ENV === "production") {
+  // Static folder
+  app.use(express.static(__dirname + "/public/"));
+
+  // Handle SPA
+  app.get(/.*/, (req, res) => res.sendFile(__dirname + "/public/index.html"));
+}
+
 const port = process.env.PORT || 14045;
 
 app.listen(port, console.log(`Edo App running in ${port} cuy!`));
